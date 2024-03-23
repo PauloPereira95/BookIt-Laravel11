d6f78f273c017b6864fcdf05365956b4ada7e4cb
JorgePereira_95 <jorge.pereira@webcomum.com>
2024-03-23 11:18:22 +0000

    feat : add template to the project and fix icons

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,24 +1,21 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-PT">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ecommerce Tailwind</title>
-
-    <link rel="shortcut icon" href="assets/images/favicon/favicon.ico" type="image/x-icon">
-
+    {{--    Fav icon  --}}
     <link rel="stylesheet" href="{{asset('assets/css/main.css')}}">
-{{--    <link rel="stylesheet" href="assets/css/main.css">--}}
-
+    <link href="{{ asset('css/all.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link
         href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&family=Roboto:wght@400;500;700&display=swap"
         rel="stylesheet">
 
-    <link href="{{ asset('css/all.css') }}" rel="stylesheet">
 
 
 </head>
