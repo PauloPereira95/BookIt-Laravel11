b1d4e539e19de24fea9e234550d1e2e8e4a702c4
PaulPereira95 <paulohuawei95@gmail.com>
2024-03-24 19:37:18 +0000

    fix: Change png on features section

--- resources/views/admin/partials/features.blade.php
+++ resources/views/admin/partials/features.blade.php
@@ -2,22 +2,22 @@
 <div class="container py-14">
     <div class="w-10/12 grid grid-cols-1 md:grid-cols-3 gap-6 mx-auto justify-center">
         <div class="border border-primary rounded-sm px-3 py-6 flex justify-center items-center gap-5">
-            <img src="assets/images/icons/delivery-van.svg" alt="Delivery" class="w-12 h-12 object-contain">
+            <img src="assets/images/icons/cutting.png" alt="Delivery" class="w-12 h-12 object-contain">
             <div>
                 <h4 class="font-medium capitalize text-lg">Amigo do Ambiente</h4>
                 <p class="text-gray-500 text-sm">Menos livros comprados significam menos recursos consumidos</p>
             </div>
         </div>
         <div class="border border-primary rounded-sm px-3 py-6 flex justify-center items-center gap-5">
-            <img src="assets/images/icons/money-back.svg" alt="Delivery" class="w-12 h-12 object-contain">
+            <img src="assets/images/icons/worldwide.png" alt="Delivery" class="w-12 h-12 object-contain">
             <div>
-                <h4 class="font-medium capitalize text-lg">Economia de custos</h4>
+                <h4 class="font-medium capitalize text-lg">Redução de custos</h4>
                 <p class="text-gray-500 text-sm">Alugar um livro geralmente é mais barato do que comprar, especialmente
                     se você planeja lê-lo apenas uma vez.</p>
             </div>
         </div>
         <div class="border border-primary rounded-sm px-3 py-6 flex justify-center items-center gap-5">
-            <img src="assets/images/icons/service-hours.svg" alt="Delivery" class="w-12 h-12 object-contain">
+            <img src="assets/images/icons/distribution-center.png" alt="Delivery" class="w-12 h-12 object-contain">
             <div>
                 <h4 class="font-medium capitalize text-lg">Espaço de armazenamento</h4>
                 <p class="text-gray-500 text-sm">Não precisa se preocupar com o espaço na estante, pois pode devolver o
